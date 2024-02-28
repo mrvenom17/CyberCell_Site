dec0c1b50d26fa782249df6fa7dfe3e38b096e25
bigyan224 <bigyanacharya224@gmail.com>
2024-02-28 16:28:59 +0530

    animations on past events

---
+++ src/javascripts/animations.js
@@ -0,0 +1,143 @@
+var scroll = new LocomotiveScroll({
+  el: document.querySelector('.main'),
+  smooth: true,
+  smartphone: {
+     smooth: true,
+     smoothing: 0.90,
+     smoothMobile:true,
+ },
+ tablet: {
+     smooth: true
+ }})
+function locomotiveanimation(){
+  gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+el: document.querySelector(".main"),
+smooth: true,
+
+// for tablet smooth
+tablet: { smooth: true },
+
+// for mobile
+smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+scrollTop(value) {
+  return arguments.length
+    ? locoScroll.scrollTo(value, 0, 0)
+    : locoScroll.scroll.instance.scroll.y;
+},
+getBoundingClientRect() {
+  return {
+    top: 0,
+    left: 0,
+    width: window.innerWidth,
+    height: window.innerHeight
+  };
+}
+
+// follwoing line is not required to work pinning on touch screen
+
+/* pinType: document.querySelector(".main").style.transform
+  ? "transform"
+  : "fixed"*/
+});
+
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+
+locomotiveanimation()
+
+function cursoranimatioon(){
+  let cursor = document.querySelector(".cursor");
+let swiper_container=document.querySelector(".past-events-container");
+  swiper_container.addEventListener("mouseenter",function(dets){
+    swiper_container.style.cursor="none";
+   gsap.to(cursor,{
+    opacity:0.8,
+    scale:1,
+    transform: 'translate(-50%,-50%)',
+    // duration:0.5,
+   })
+  });
+  swiper_container.addEventListener("mousemove",function(dets){
+   gsap.to(cursor,{
+    left:dets.pageX,
+    top:dets.pageY,
+   });
+});
+swiper_container.addEventListener("mouseleave",function(dets){
+  swiper_container.style.cursor="none";
+ gsap.to(cursor,{
+  opacity:0,
+  scale:0,
+  transform: 'translate(-50%,-50%)',
+  // duration:0.5,
+ })
+});
+
+}
+cursoranimatioon();
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: false,
+  centeredSlides: 1,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      // When window width is <= 700px
+      700: {
+          slidesPerView: 3,
+      },
+      // More breakpoints can be added if needed
+  },
+});
+
+
+function eventanimation() {
+  var events = document.querySelectorAll('.event');
+
+  events.forEach(event => {
+    console.log(event);
+    gsap.from(event, {
+      y:"100%",
+      opacity:0,
+      duration:2,
+      scrollTrigger: {
+        trigger: '.page4',
+        toggleActions:"restart none reverse none",
+        scroller: '.main',
+        start: 'top 40%',
+        end: '10% 50%',
+        stagger: true,
+      },
+    });
+  });
+}
+
+window.onload = eventanimation;
+
