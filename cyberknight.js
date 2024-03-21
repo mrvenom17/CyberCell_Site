3a225b3d0784f5043768e68e84e48ae3c6ed94ef
bigyan224 <bigyanacharya224@gmail.com>
2024-03-21 23:33:14 +0530

    added ycf

--- cyberknight.js
+++ cyberknight.js
@@ -26,3 +26,10 @@ updateCountdown();
 
 // Update countdown every second
 setInterval(updateCountdown, 1000);
+
+VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+});
